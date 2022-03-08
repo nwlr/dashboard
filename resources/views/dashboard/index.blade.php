8145eec804f25b70b4c39131ca224c471d557d13
Philippe <mbp@MacBook-Pro.local>
2022-03-08 14:56:22 +0100

    wip

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,69 @@
+<x-guest-layout>
+    <div class="grid grid-cols-5 gap-4">
+        <div class="col-span-5 bg-gray-100 rounded-lg p-4 justify-between flex items-center text-gray-600 font-bold">
+            <div>
+                Dienstag. 8 März 2022 - 14:50 Uhr
+            </div>
+            <div class="flex items-center">
+            <span><x-codicon-location class="h-10 w-10" /></span>
+            <span class="">Neugrütstrasse 2, 8610 Uster</span>
+            <x-fluentui-weather-partly-cloudy-day-20-o class="h-10 w-10 ml-12"/>
+            <span class="">Bewölkt, 7°</span>
+
+            </div>
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 py-12 col-span-2">
+            <h1 class="text-3xl font-bold text-gray-600 uppercase">Sitzungszimmer 1</h1>
+            <div class="text-9xl font-bold mt-4 text-lime-600">Frei</div>
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 py-12 col-span-2">
+            <h1 class="text-lg font-thin text-gray-600 uppercase">Kalender</h1>
+            <div class="text-2xl font-bold mt-4 text-gray-600">15:10 Besprechnung Jürg Artho</div>
+            <div class="text-2xl font-bold mt-2 text-gray-600">16:30 Testtermin</div>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center items-center py-12">
+            <svg class="h-32" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 97">
+                <g data-name="Gruppe 659">
+                    <g data-name="Gruppe 626">
+                        <path data-name="Pfad 381" d="M21.275 13.7c0-4.354-2.57-6.924-6.995-6.924a27.92 27.92 0 00-6.924.928v26.2a3.676 3.676 0 11-7.351 0V6.21a4.519 4.519 0 013.14-4.354A34.057 34.057 0 0114.281 0a13.986 13.986 0 0110.35 4 14.218 14.218 0 014 10.279V33.9a3.686 3.686 0 01-3.712 3.711 3.627 3.627 0 01-3.64-3.711z"></path>
+                        <path data-name="Pfad 382" d="M55.895 15.489c0-5.924-2.427-8.708-6.995-8.708-4.5 0-6.924 2.712-6.924 8.708zM41.976 22.27c0 2.926.5 4.854 1.428 6.138a6.587 6.587 0 005.5 2.427 19.12 19.12 0 007.708-1.856 3.84 3.84 0 011.5-.357 3.524 3.524 0 013.5 3.355 3.4 3.4 0 01-1.856 3.212 24.839 24.839 0 01-10.849 2.427 13.593 13.593 0 01-14.286-14.347v-8.994A13.587 13.587 0 0148.901 0 13.985 13.985 0 0159.25 4a14.217 14.217 0 014 10.279v3.355a4.631 4.631 0 01-4.64 4.64z"></path>
+                        <path data-name="Pfad 383" d="M67.603 4.925a3.625 3.625 0 01-.214-1.214A3.764 3.764 0 0171.029 0a3.66 3.66 0 013.5 2.427l7.781 23.341 7.209-23.269A3.667 3.667 0 0192.94 0a3.792 3.792 0 013.784 3.711 3.244 3.244 0 01-.214 1.143L86.376 34.69a4.222 4.222 0 01-4 2.927 4.573 4.573 0 01-3.925-2.784z"></path>
+                        <path data-name="Pfad 384" d="M121.139 21.414h-7.353c-6.21 0-6.567 3.355-6.567 4.568 0 1.284.357 4.854 6.567 4.854a31.429 31.429 0 007.352-.928zm7.352 9.993a4.611 4.611 0 01-3.141 4.354 36.025 36.025 0 01-11.564 1.856c-4.354 0-7.78-1.071-10.207-3.14a10.764 10.764 0 01-3.711-8.494 10.085 10.085 0 013.711-8.209c2.427-2.07 5.853-3.14 10.207-3.14h7.352v-2.5c0-3-2.213-5.353-5.925-5.353a12.954 12.954 0 00-7.709 1.927 4.126 4.126 0 01-2 .643 3.36 3.36 0 01-3.5-3.212 3.727 3.727 0 011.785-3.284C107.644.642 110.998 0 116.137 0c7.994 0 12.349 5.782 12.349 12.135z"></path>
+                        <path data-name="Pfad 385" d="M.004 49.273a3.676 3.676 0 117.352 0v44.041a3.676 3.676 0 11-7.352 0z" fill="#ffe501"></path>
+                        <path data-name="Pfad 386" d="M35.047 63.12a3.627 3.627 0 013.64-3.711 3.686 3.686 0 013.712 3.711v27.695a4.612 4.612 0 01-3.141 4.355 34.177 34.177 0 01-11.206 1.856c-4.14 0-7.852-1.214-10.493-3.855-2.569-2.569-3.783-6.281-3.783-10.493V63.12a3.676 3.676 0 117.351 0v20.129c0 4.426 2.57 6.924 6.924 6.924a28.021 28.021 0 006.995-.857z" fill="#ffe501"></path>
+                        <path data-name="Pfad 387" d="M62.744 84.32l-7.566 11.135a3.724 3.724 0 01-3 1.57 3.779 3.779 0 01-3.711-3.711 3.721 3.721 0 01.642-2.07l9.35-13.2-9.136-12.848a3.721 3.721 0 01-.642-2.07 3.779 3.779 0 013.711-3.711 3.724 3.724 0 013 1.57l7.352 10.778 7.351-10.778a3.69 3.69 0 016.71 2.141 3.718 3.718 0 01-.643 2.07l-9.136 12.838 9.35 13.2a3.317 3.317 0 01.714 2.07 3.794 3.794 0 01-3.783 3.711 3.541 3.541 0 01-3-1.57z" fill="#ffe501"></path>
+                    </g>
+                </g>
+            </svg>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center flex-col items-center">
+            <div class="text-6xl font-bold mt-4 text-gray-600 ">0</div>
+            <h1 class="text-lg font-thin text-gray-600 uppercase">Personen im Raum</h1>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center flex-col items-center">
+            <div class="text-6xl font-bold mt-4 text-lime-600 ">20°</div>
+            <h1 class="text-lg font-thin text-gray-600 uppercase">Temperatur</h1>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center flex-col items-center">
+            <div class="text-6xl font-bold mt-4 text-red-600">66%</div>
+            <h1 class="text-lg font-thin text-gray-600 uppercase">Luftfeuchtigkeit</h1>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center flex-col items-center">
+            <div class="text-6xl font-bold mt-4 text-lime-600 ">1,5</div>
+            <h1 class="text-lg font-thin text-gray-600 uppercase">VOC</h1>
+
+        </div>
+        <div class="bg-gray-100 rounded-lg p-4 flex justify-center flex-col items-center">
+            <div class="text-6xl font-bold mt-4 text-orange-600 ">990pm</div>
+            <h1 class="text-lg font-thin text-gray-600 uppercase">Co2</h1>
+
+        </div>
+
+
+    </div>
+</x-guest-layout>
